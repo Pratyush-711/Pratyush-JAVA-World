eaaf0c556f478a7a8d7e7e5cb935f3363ae69ec4
Pratyush-711 <151891936+Pratyush-711@users.noreply.github.com>
2024-06-05 23:00:55 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class Add {
+    public static void main(String[] args) {
+        System.out.println("Addition of three numbers");
+        int num1 = 6;
+        int num2 = 5;
+        int num3 = 8;
+        int sum;
+        sum = num1+num2+num3;
+        System.out.println("The sum of three numbers are:"+sum);
+    }
+}
