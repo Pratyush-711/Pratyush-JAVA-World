50abe7f1b4ed3aedbc845c518b676f132cb49a22
Pratyush-711 <151891936+Pratyush-711@users.noreply.github.com>
2024-06-05 22:53:00 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+/*
+1. AddTwoNumber --> pascal naming convention(Classes)
+2. addTwoNumber --> camel case naming convention(Functions)
+*/
