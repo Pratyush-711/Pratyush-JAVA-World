03bbea10bdd9a786fe6a34c0620b565799e68b4f
Pratyush-711 <151891936+Pratyush-711@users.noreply.github.com>
2024-06-06 10:39:10 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,20 @@
+public class Literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 56;
+        char ch ='A';
+        float f1 = 16.7f;
+        double d1 = 4.66D;
+        long ageDino = 56666666666L;
+        boolean a = true;
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(age2);
+        System.out.println(ageDino);
+        System.out.println(a);
+        String str = "Pratyush Manivansh";
+        System.out.println(str); 
+    }
+}
